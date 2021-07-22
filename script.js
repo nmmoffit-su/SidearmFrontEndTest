24a5a0a03a1a3350ec53b8237daf7258e49653cc
-- <nmmoffit@syr.edu>
2021-07-22 18:31:16 -0400

    Tech Test 2021 Revisions

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 function navViewModel() {
-    self.tabs = ko.observableArray([
-        { title: "Teams" },
-        { title: "Tickets" },
-        { title: "Patriot Club" },
-        { title: "Inside Athletics" },
-        { title: "Fan Central" },
-    ]);
-    self.dropdownContent = ko.observableArray([
-        { subtitle: "Men's Sports" },
-        { subtitle: "Women's Sports" }
-    ]);
-    self.mensport = ko.observableArray([
-        { sport: "Baseball" },
-        { sport: "Basketball" },
-        { sport: "Cross Country" }
-    ])
-    self.womsport = ko.observableArray([
-        { sport: "Basketball" },
-        { sport: "Cross Country" }
-    ])
+    tabs = [
+        "Teams", "Tickets", "Patriot Club", "Inside Athletics", "Fan Central"
+    ];
+    dropdownContent = [
+        { subtitle: "Men's Sports", sport: ["Baseball", "Basketball", "Cross Country"] },
+        { subtitle: "Women's Sports", sport: ["Basketball", "Cross Country"] }
+    ];
 }
